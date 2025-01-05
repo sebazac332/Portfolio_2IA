7accef7925dfe11d8c6cbc5c3a225837918f40bd
Sebastian Zuzunaga <seb55381@hotmail.com>
2025-01-05 00:01:07 -0500

    beam search algorithm added

---
+++ algorithms/bidirectional_bfs.py
@@ -0,0 +1,63 @@
+from collections import deque
+
+def word_ladder_bidirectional_bfs(begin_word, end_word, word_list):
+
+    if end_word not in word_list:
+        return 0
+
+    word_list = set(word_list)
+    word_list.add(begin_word)
+
+    begin_queue = deque([(begin_word, 1)])
+    end_queue = deque([(end_word, 1)])
+
+    begin_visited = {begin_word: 1}
+    end_visited = {end_word: 1}
+
+    while begin_queue and end_queue:
+        result = bfs_step(begin_queue, begin_visited, end_visited, word_list)
+        if result:
+            return result
+
+        result = bfs_step(end_queue, end_visited, begin_visited, word_list)
+        if result:
+            return result
+
+    return 0
+
+def bfs_step(queue, visited, other_visited, word_list):
+
+    current_word, level = queue.popleft()
+
+    for i in range(len(current_word)):
+        for char in 'abcdefghijklmnopqrstuvwxyz':
+            transformed_word = current_word[:i] + char + current_word[i+1:]
+
+            if transformed_word in word_list:
+                if transformed_word in other_visited:
+                    return level + other_visited[transformed_word]
+
+                if transformed_word not in visited:
+                    visited[transformed_word] = level + 1
+                    queue.append((transformed_word, level + 1))
+
+    return None
+
+if __name__ == "__main__":
+    
+    word_list = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+    print("Words in word list: ")
+
+    for word in word_list:
+        print(word)
+
+    begin_word = input("Enter the begin word: ").strip().lower()
+    end_word = input("Enter the end word: ").strip().lower()
+
+    result = word_ladder_bidirectional_bfs(begin_word, end_word, word_list)
+    
+    if result:
+        print(f"The shortest transformation sequence length is: {result}")
+    else:
+        print("No valid transformation sequence exists.")
--- docs/references_first.md
+++ docs/references_first.md
@@ -13,3 +13,12 @@
 - https://www.simplilearn.com/tutorials/artificial-intelligence-tutorial/heuristic-function-in-ai accessado 22/12/2024
 - https://hackmd.io/@coding-guy/SJsVB8JGT?utm_source=preview-mode&utm_medium=rec accessado 25/12/2024
 - https://www.cognizant.com/us/en/glossary/evolutionary-algorithm accessado 28/12/2024
+
+# Referências de algoritmos
+## Bidirectional BFS
+- https://www.youtube.com/watch?v=RS9r7FzYKDk accesado 02/01/2025
+- https://leetcode.com/problems/word-ladder/solutions/ accesado 03/01/2025
+
+## Beam search
+- https://blog.finxter.com/python-beam-search-algorithm/ accesado 04/01/2025
+- https://www.youtube.com/watch?v=3m1lvN2fTtY accesado 04/01/2025
